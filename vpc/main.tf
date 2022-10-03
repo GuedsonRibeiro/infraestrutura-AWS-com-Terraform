0bb63d02bf6a25312faae2fa64348f3a8b4dae5f
Guedson Ribeiro rm96055 <guedson.ge@gmail.com>
2022-10-02 23:32:54 -0300

    07

--- vpc/main.tf
+++ vpc/main.tf
@@ -106,7 +106,7 @@ resource "aws_vpc_endpoint" "ec2" {
   service_name = "com.amazonaws.us-east-1.ec2"
   vpc_endpoint_type = "Interface"
 
-  security_group_ids = var.sg_pub
+  security_group_ids = var.sg_pub_id
 
 }
 
