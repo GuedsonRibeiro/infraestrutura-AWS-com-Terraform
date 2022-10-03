ea407390f58c92e38a8e5c5cb0b73b00278d5be6
Guedson Ribeiro rm96055 <guedson.ge@gmail.com>
2022-10-03 01:29:20 -0300

    22

--- vpc/main.tf
+++ vpc/main.tf
@@ -118,5 +118,5 @@ resource "aws_sns_topic" "topic" {
 resource "aws_sns_topic_subscription" "email-target" {
   topic_arn = aws_sns_topic.topic.arn
   protocol  = "email"
-  endpoint  = "guedson.ge@gmail.com"
+  endpoint  = "guedson.ge@gmail.com
 }
